152b3a49938e613f8d69a670e6dd64e3184c280c
guyburin <burinpanchat@gmail.com>
2021-12-22 23:12:20 +0700

    working with num,str,boolean

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-function add(n1, n2) {
-    return n1 + n2;
+function add(n1, n2, showResult) {
+    //   if (typeof n1 !== "number" || typeof n2 !== "number") {
+    //     throw new Error("Incorrect input!!");
+    //   }
+    if (showResult) {
+        console.log(n1 + n2);
+    }
+    else {
+        return n1 + n2;
+    }
 }
-var number1 = "5";
+var number1 = 5;
 var number2 = 2.8;
-var result = add(number1, number2);
-console.log(result); // 7.8
+var printResult = true;
+add(number1, number2, printResult);
