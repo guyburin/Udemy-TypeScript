254dfb5ba9e4b961b0636e5000b12a1d052e56f2
guyburin <burinpanchat@gmail.com>
2021-12-22 23:52:09 +0700

    objs-arrays-enums

--- objs-arrays-enums.ts
+++ objs-arrays-enums.ts
@@ -13,32 +13,39 @@
 //   hobbies: ["game", "sports"],
 //   role: [2, "author"],
 // };
+
 // person.role.push("admin");
 // person.role[1] = 10; error !
+
 // person.role = [0, "admin", "user"];
+
 // const admin = 0;
 // const read_only = 1;
 // const author = 2;
-var Role;
-(function (Role) {
-    Role["admin"] = "admin";
-    Role[Role["read_only"] = 100] = "read_only";
-    Role["author"] = "author";
-})(Role || (Role = {}));
-var person = {
-    name: "Guy",
-    age: 23,
-    hobbies: ["game", "sports"],
-    role: Role.admin
-};
-var favoriteActivity;
-favoriteActivity = ["sports"];
-console.log(person.name);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toLocaleUpperCase());
-    //   console.log(hobby.map())
+
+enum Role {
+  admin = "admin",
+  read_only = 100,
+  author = "author",
 }
+
+const person = {
+  name: "Guy",
+  age: 23,
+  hobbies: ["game", "sports"],
+  role: Role.admin,
+};
+
+let favoriteActivity: string[];
+favoriteActivity = ["sports"];
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toLocaleUpperCase());
+  //   console.log(hobby.map())
+}
+
 if (person.role === Role.author) {
-    console.log("is author");
+  console.log("is author");
 }
